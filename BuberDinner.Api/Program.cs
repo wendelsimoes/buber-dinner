e123150632a61fab7f3cfc0d8d8670f437ae2954
Wendel Simões dos Santos <wendelsimoes.pessoal@gmail.com>
2025-10-28 16:45:03 -0300

    feat: set project structure

---
+++ BuberDinner.Api/Program.cs
@@ -0,0 +1,17 @@
+using BuberDinner.Application;
+using BuberDinner.Infraestructure;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services
+        .AddApplication()
+        .AddInfrastructure();
+    builder.Services.AddControllers();
+}
+
+var app = builder.Build();
+{
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+}
