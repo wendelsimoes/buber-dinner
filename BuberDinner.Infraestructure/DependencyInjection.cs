6cb9999568b0c2f1d4b55afb2e55ec58fbe99250
Wendel Simões dos Santos <wendelsimoes.pessoal@gmail.com>
2025-10-28 22:07:40 -0300

    feat: set repository pattern

--- BuberDinner.Infraestructure/DependencyInjection.cs
+++ BuberDinner.Infraestructure/DependencyInjection.cs
@@ -1,6 +1,8 @@
 using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Persistence;
 using BuberDinner.Application.Common.Interfaces.Services;
 using BuberDinner.Infraestructure.Authentication;
+using BuberDinner.Infraestructure.Persistence;
 using BuberDinner.Infraestructure.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +20,7 @@ public static class DependencyInjection
         services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 
+        services.AddScoped<IUserRepository, UserRepository>();
         return services;
     }
 }
