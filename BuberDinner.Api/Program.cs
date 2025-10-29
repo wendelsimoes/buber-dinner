19351a2feba46ec0dc33578a43ecf43cfcfe40f8
Wendel Simões dos Santos <wendelsimoes.pessoal@gmail.com>
2025-10-29 09:49:53 -0300

    feat: set global error handling

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,17 +1,27 @@
+using BuberDinner.Api.Errors;
+using BuberDinner.Api.Filters;
 using BuberDinner.Application;
 using BuberDinner.Infraestructure;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
-        
+
+    // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
+
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
